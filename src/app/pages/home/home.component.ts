0d9e92a62bc6cf062735b5b02fe13259912aec83
PabloSnhz <pablosanhuezaaa@gmail.com>
2023-12-06 16:06:12 -0300

    arreglos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnDestroy, OnInit, inject } from '@angular/core';
-import { HeaderService } from 'src/app/core/services/header.service';
-import { CategoriasService } from '../../core/services/categorias.service';
-import { Categoria } from 'src/app/core/interfaces/categorias';
-import { TarjetaCategoriaComponent } from 'src/app/core/components/tarjeta-categoria/tarjeta-categoria.component';
+import { Component, OnDestroy, OnInit, WritableSignal, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
+import { CategoriasService } from '../../core/services/categorias.service';
+import { HeaderService } from 'src/app/core/services/header.service';
+import { Categoria } from 'src/app/core/interfaces/categorias';
+import { TarjetaCategoriaComponent } from 'src/app/core/components/tarjeta-categoria/tarjeta-categoria.component';
 
 @Component({
   selector: 'app-home',
@@ -17,12 +18,12 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   headerService = inject(HeaderService);
   categoriasService = inject(CategoriasService);
-  categorias: Categoria[] = [];
+  categorias: WritableSignal<Categoria[]> = signal([]);
 
   ngOnInit(): void {
     this.headerService.titulo.set('Home')
     this.headerService.extension.set(true)
-    this.categoriasService.getAll().then(res => this.categorias = res);
+    this.categoriasService.getAll().then(res => this.categorias.set(res));
   }
 
   ngOnDestroy(): void {
